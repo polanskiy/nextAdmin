6144f04f894faa1f400521ff29a57fe214e56dbd
polanskiy <polanskiy.html@gmail.com>
2019-08-05 18:16:39 +0300

    tabs

--- .next/static/chunks/styles.js
+++ .next/static/chunks/styles.js
@@ -1,79 +1,5 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([["styles"],{
 
-/***/ "./node_modules/react-web-tools/dist/carousel/carousel.css":
-/*!*****************************************************************!*\
-  !*** ./node_modules/react-web-tools/dist/carousel/carousel.css ***!
-  \*****************************************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-// extracted by mini-css-extract-plugin;
-    if (true) {
-      var injectCss = function injectCss(prev, href) {
-        var link = prev.cloneNode();
-        link.href = href;
-        link.onload = function() {
-          prev.parentNode.removeChild(prev);
-        };
-        prev.stale = true;
-        prev.parentNode.insertBefore(link, prev);
-      };
-      module.hot.dispose(function() {
-        window.__webpack_reload_css__ = true;
-      });
-      if (window.__webpack_reload_css__) {
-        module.hot.__webpack_reload_css__ = false;
-        console.log("[HMR] Reloading stylesheets...");
-        var prefix = document.location.protocol + '//' + document.location.host;
-        document
-          .querySelectorAll("link[href][rel=stylesheet]")
-          .forEach(function(link) {
-            if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1565009763136");
-          });
-      }
-    }
-  
-
-/***/ }),
-
-/***/ "./node_modules/react-web-tools/dist/tabs/tabs.css":
-/*!*********************************************************!*\
-  !*** ./node_modules/react-web-tools/dist/tabs/tabs.css ***!
-  \*********************************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-// extracted by mini-css-extract-plugin;
-    if (true) {
-      var injectCss = function injectCss(prev, href) {
-        var link = prev.cloneNode();
-        link.href = href;
-        link.onload = function() {
-          prev.parentNode.removeChild(prev);
-        };
-        prev.stale = true;
-        prev.parentNode.insertBefore(link, prev);
-      };
-      module.hot.dispose(function() {
-        window.__webpack_reload_css__ = true;
-      });
-      if (window.__webpack_reload_css__) {
-        module.hot.__webpack_reload_css__ = false;
-        console.log("[HMR] Reloading stylesheets...");
-        var prefix = document.location.protocol + '//' + document.location.host;
-        document
-          .querySelectorAll("link[href][rel=stylesheet]")
-          .forEach(function(link) {
-            if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1565009763121");
-          });
-      }
-    }
-  
-
-/***/ }),
-
 /***/ "./styles/main.scss":
 /*!**************************!*\
   !*** ./styles/main.scss ***!
@@ -103,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1565009760774");
+            injectCss(link, link.href.split("?")[0] + "?unix=1565018087817");
           });
       }
     }
--- views/site/pages/Travels/TravelAbout.js
+++ views/site/pages/Travels/TravelAbout.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Tabs } from 'react-web-tools';
+import Tabs from '../../elements/Tabs';
 
 const TravelAbout = () => (
   <div className="travelAboutBox">
     <div className="contentWrapper">
       <Tabs>
         <div name="Климат">климат</div>
-        <div name="Причины поехать">климат</div>
-        <div name="Трансфер">климат</div>
-        <div name="Места для посещений">климат</div>
-        <div name="Выпить / Поесть">климат</div>
+        <div name="Причины поехать">qwewqeqwe</div>
+        <div name="Трансфер">qeqww</div>
+        <div name="Места для посещений">3123</div>
+        <div name="Выпить / Поесть">fsdfsd</div>
       </Tabs>
     </div>
   </div>
