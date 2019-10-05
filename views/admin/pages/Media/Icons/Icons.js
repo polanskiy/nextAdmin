429ba0c586ee469db26ae0557851547aaa9f8e1e
unknown <polanskiy.html@gmail.com>
2019-10-06 00:23:58 +0300

    refresh request icons

--- views/admin/pages/Media/Icons/Icons.js
+++ views/admin/pages/Media/Icons/Icons.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import request from '../../../../../utils/request';
 import NewIcon from './NewIcon';
 
 const Icons = () => {
@@ -8,10 +8,7 @@ const Icons = () => {
   const fetchImages = async () => {
     try {
       setIconList({ data: [], isFetching: true });
-      const res = await axios({
-        method: 'get',
-        url: 'http://localhost:3000/api/images/icon',
-      });
+      const res = await request('/api/images/icon', 'get');
       console.log('res', res);
       if (isMount) setIconList({ data: res.data, isFetching: false });
     } catch (e) {
@@ -27,19 +24,13 @@ const Icons = () => {
     };
   }, []);
 
-  const deleteImage = (e) => {
+  const deleteImage = async (e) => {
     const { name } = e.currentTarget.dataset;
     console.dir(name);
-    axios({
-      method: 'delete',
-      url: '/api/images',
-      data: {
-        filename: name,
-      },
-    })
-      .then(() => {
-        fetchImages();
-      });
+    await request('/api/images', 'delete', {
+      filename: name,
+    });
+    fetchImages();
   };
 
   const renderIcons = () => iconList.data.map(img => (
